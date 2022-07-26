851619c86d453605f004a500c7b7b7a0bd22bf73
Christopher Beckham <christopher.j.beckham@gmail.com>
2022-07-25 22:31:21 -0400

    Only switch windows if a choice has actually been made

--- choosem/cli/focus.py
+++ choosem/cli/focus.py
@@ -6,6 +6,7 @@ import click
 from choosem.utils import choose
 
 
+
 @click.command()
 def focus():
     """focus a program window"""
@@ -14,5 +15,6 @@ def focus():
         capture_output=True).stdout.decode()
     windows = json.loads(windows)
     choice = choose('\n'.join([f"{w['app']}: {w['title']}" for w in windows if w.get('title')]), index=True)
-    choice = windows[choice]
-    subprocess.run(['yabai', '-m', 'window', '--focus', str(choice["id"])])
+    if choice != -1:
+        choice = windows[choice]
+        subprocess.run(['yabai', '-m', 'window', '--focus', str(choice["id"])])
